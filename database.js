3d96ae84dc1297a0a68f6b0ce4ead486921b425e
Adarsha <adarsha69005@gmail.com>
2018-12-16 09:01:58 +0545

    changed the database status

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const db = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : 'Skulllink01',
+      password : 'test',
       database : 'bookshop'
     }
   });
--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : 'Skulllink01',
+      password : 'test',
       database : 'bookshop'
     }
   },
